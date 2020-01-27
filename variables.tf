f77f8af53f141cdb8df0226cbb14cbc90b1f0575
joel <joelcote68@gmail.com>
2020-01-27 13:49:14 -0600

    testing

--- variables.tf
+++ variables.tf
@@ -1,11 +1,3 @@
-variable "network" {
-  description = "network the firewall rule will be deployed in"
-}
-
-variable "project_id" {
-  description = "project id for the firewall rules"
-}
-
 variable "target_tags" {
   description = "what instances will this firewall rule be applied to"
 }
